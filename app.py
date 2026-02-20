b7d6496a7cfe9b51bc0c3a6f6f298624324a260c
Satyavardhan Koyalkar <satyavardhan24105@gmail.com>
2026-02-20 23:17:28 +0530

    switch to gunicorn

--- app.py
+++ app.py
@@ -87,9 +87,3 @@ def index():
         error=error
     )
 
-# =============================
-# RENDER COMPATIBLE RUN
-# =============================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
